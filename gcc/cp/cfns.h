981f3ceb3c9ae66b41c68549aff8297725de6839
chuno <Chief Unoriginal nobody@example.com>
2019-01-10 17:38:29 +0000

    Updated cfns.gperf to avoid incompatible decls

--- gcc/cp/cfns.h
+++ gcc/cp/cfns.h
@@ -53,8 +53,11 @@ __inline
 static unsigned int hash (const char *, unsigned int);
 #ifdef __GNUC__
 __inline
+#if defined __GNUC_STDC_INLINE__ || defined __GNUC_GNU_INLINE__
+__attribute__ ((__gnu_inline__))
 #endif
-const char * libc_name_p (const char *, unsigned int);
+#endif
+static const char * libc_name_p (const char *, unsigned int);
 /* maximum key range = 391, duplicates = 0 */
 
 #ifdef __GNUC__
@@ -124,7 +127,7 @@ hash (register const char *str, register unsigned int len)
 
 #ifdef __GNUC__
 __inline
-#ifdef __GNUC_STDC_INLINE__
+#if defined __GNUC_STDC_INLINE__ || defined __GNUC_GNU_INLINE__
 __attribute__ ((__gnu_inline__))
 #endif
 #endif
